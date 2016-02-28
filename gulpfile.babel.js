2755334c0ffa8f44ec4f9ce161b2191fc1305e3c
typhonrt <support@typhonrt.org>
2016-02-28 14:41:02 -0800

    0.0.3 release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,11 @@
 /**
  * Please see `typhonjs-core-gulptasks` (https://www.npmjs.com/package/typhonjs-core-gulptasks)
  */
-/* eslint-disable */
+import gulp       from 'gulp';
+import gulpTasks  from 'typhonjs-core-gulptasks';
 
-var gulp = require('gulp');
-
-// Require all tasks and set `rootPath` to the base project path and `srcGlob` to all JS sources in `./src`.
-require('typhonjs-core-gulptasks')(gulp,
+// Import all tasks and set `rootPath` to the base project path and `srcGlob` to all JS sources in `./src`.
+gulpTasks(gulp,
 {
    rootPath: __dirname,
    importTasks: ['esdoc', 'eslint', 'npm'],
